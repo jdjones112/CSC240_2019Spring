51de777a6d44c3cc9f2bde952587c743b71819ef
Si Chen <quake0day@gmail.com>
2019-02-06 10:20:12 -0500

    change class name

--- src/Chapter9/PokemonViewer.java
+++ src/Chapter9/PokemonViewer.java
@@ -2,7 +2,7 @@ package Chapter9;
 
 import java.io.IOException;
 
-public class PokemonName {
+public class PokemonViewer {
     public static void main(String[] args) throws IOException {
         String ID = "1";
         System.out.println(PokemonDataAPI.getData(ID));
