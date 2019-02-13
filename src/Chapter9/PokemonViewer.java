86429e230fadf25dde63b4cff021b2c49cf30f77
Si Chen <quake0day@gmail.com>
2019-02-13 09:59:59 -0500

    change class name

--- src/Chapter9/PokemonViewer.java
+++ src/Chapter9/PokemonViewer.java
@@ -4,8 +4,19 @@ import java.io.IOException;
 
 public class PokemonViewer {
     public static void main(String[] args) throws IOException {
-        String ID = "1";
-        System.out.println(PokemonDataAPI.getData(ID));
+        String ID = "344";
+        String rawData = PokemonDataAPI.getData(ID);
+        // Extract the name of the Pokemon
+        // From the raw data.
+        int startLocation = rawData.indexOf("\"name\":");
+        int endLocation = rawData.indexOf(",\"names");
+        startLocation = startLocation + 8;
+        endLocation = endLocation - 1;
+        String name = rawData.substring(startLocation, endLocation);
+        System.out.println(name);
+        String URLTest = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png";
+        PokemonDataAPI.displayImageFromURL(URLTest);
+
     }
 
 }
